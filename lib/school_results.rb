570110429db520b77808b49b49085228707a62ce
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-18 18:56:40 +0100

    refactored

--- lib/school_results.rb
+++ lib/school_results.rb
@@ -1,9 +1,8 @@
 
 def calculate_results(input)
-  input = input.split(",").map(&:strip)
   hash = Hash.new(0)
-  input.each { |key| hash[key] += 1 }
   result = []
+  input.split(",").map(&:strip).each { |key| hash[key] += 1 }
   hash.each { |key, value| result.push("#{key}: #{value}") }
   return result.join("\n")
 end
