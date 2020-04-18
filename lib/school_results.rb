3c86589b00e9c1e1b60e58507f6072a111c35c94
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-18 13:48:22 +0100

    Passing second test

--- lib/school_results.rb
+++ lib/school_results.rb
@@ -1,8 +1,9 @@
 
 def calculate_results(input)
-  input = input.split(",")
+  input = input.split(",").map(&:strip)
   hash = Hash.new(0)
   input.each { |key| hash[key] += 1 }
+  p hash
   hash.each do |key, value |
     return "#{key}: #{value}"
   end
