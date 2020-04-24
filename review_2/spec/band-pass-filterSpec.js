7447bb59489ca165c7d45cff0fd9226d6fdc2a68
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-24 11:09:41 +0100

    Added test for 2 unfiltered numbers

--- review_2/spec/band-pass-filterSpec.js
+++ review_2/spec/band-pass-filterSpec.js
@@ -9,4 +9,14 @@ describe('Band Pass', () => {
     let bandPassFilter = new BandPassFilter([10], 40, 50);
     expect(bandPassFilter.changeFrequency()).toEqual([40])
   })
+
+  it('has to round down 1 value', () => {
+    let bandPassFilter = new BandPassFilter([12], 8, 10);
+    expect(bandPassFilter.changeFrequency()).toEqual([10])
+  })
+
+  it('has 2 unfiltered values', () => {
+    let bandPassFilter = new BandPassFilter([12, 14], 10, 15);
+    expect(bandPassFilter.changeFrequency()).toEqual([12, 14])
+  })
 })
--- review_2/src/band-pass-filter.js
+++ review_2/src/band-pass-filter.js
@@ -3,13 +3,19 @@ class BandPassFilter {
     this._track = array;
     this._lowerRange = lowerRange;
     this._upperRange = upperRange;
+    this._newArray = [];
   }
 
   changeFrequency(){
-    if (this._track >= this._lowerRange && this._track <= this._upperRange) {
-      return this._track;
-    } else {
-      return [this._lowerRange];
-    }
-  }
+   for(var i = 0; i < this._track.length; i++){
+     if (this._track[i] >= this._lowerRange && this._track[i] <= this._upperRange) {
+       this._newArray.push(this._track[i]);
+     } else if (this._track[i] > this._upperRange) {
+       this._newArray.push(this._upperRange);
+     } else {
+       this._newArray.push(this._lowerRange);
+     }
+   }
+  return this._newArray;
+ }
 }
